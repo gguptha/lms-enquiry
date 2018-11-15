77a286d295ba95edf5efa3ece13c3fa818da2141
sajeevkhan <sajeev.khan@gmail.com>
2018-11-15 21:17:57 +0100

    Header Value X-Requested-With = 'X'

--- src/main/java/pfs/lms/enquiry/service/impl/SAPIntegrationService.java
+++ src/main/java/pfs/lms/enquiry/service/impl/SAPIntegrationService.java
@@ -76,7 +76,7 @@ public class SAPIntegrationService implements ISAPIntegrationService {
                     String authHeader = "Basic " + new String( encodedAuth );
                     set( "Authorization", authHeader );
                     setContentType(MediaType.APPLICATION_JSON);
-                    //add("X-Csrf-Token", fetchCSRFToken());
+                    add("X-Requested-With", "X");
                 }
             };
             // MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
