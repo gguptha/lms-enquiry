2a4299a8f62153c17b42be1022918cc3b6061eb9
Madhavi <madhavi@cloudloom.io>
2020-11-25 23:02:51 +0530

    https://github.com/LeanThoughts/lms-enquiry/issues/29

---
+++ src/main/java/pfs/lms/enquiry/resource/LoanContractSearchResource.java
@@ -0,0 +1,23 @@
+package pfs.lms.enquiry.resource;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class LoanContractSearchResource {
+    //private LocalDate enquiryDateFrom;
+    //private LocalDate enquiryDateTo;
+    private String partyName;
+    private String loanClass;
+    private String  financingType;
+    private Integer borrowerCodeFrom;
+    private Integer borrowerCodeTo;
+    private Integer loanNumberFrom;
+    private Integer loanNumberTo;
+    private String projectLocationState;
+    private String projectType;
+    private String assistanceType;
+    private String technicalStatus;
+    //private String rating;
+}
