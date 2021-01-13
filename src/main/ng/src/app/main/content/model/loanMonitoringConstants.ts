2c30f63e926a65181bada9413c7c3efb8f7a38e3
Gopinath Guptha <gopinath.br@gmail.com>
2021-01-13 13:33:58 +0530

    Fixed dropdown issues ...

--- src/main/ng/src/app/main/content/model/loanMonitoringConstants.ts
+++ src/main/ng/src/app/main/content/model/loanMonitoringConstants.ts
@@ -51,14 +51,52 @@ export class LoanMonitoringConstants {
 
     // collateralObjectTypes
     static collateralObjectTypes = [
-        {code: "0" , value:"Collateral Object Type 1"},
-        {code: "1" , value:"Collateral Object Type 2"}
+        {code: "Z00007" , value:"PFS:Personal Guarantee"},
+        {code: "Z00008" , value:"PFS:Corporate Guarantee"},
+        {code: "Z00009" , value:"PFS:Bank Guarantee"},
+        {code: "Z30001" , value:"Securities"},
+        {code: "ZCL001" , value:"DSRA Account"},
+        {code: "ZCL002" , value:"TRA Account"},
+        {code: "ZCL003" , value:"Other Current Assets"},
+        {code: "ZDE001" , value:"Plant and Machinery"},
+        {code: "ZDE002" , value:"Furniture and Fixtures"},
+        {code: "ZDE003" , value:"Office Equipment"},
+        {code: "ZDE004" , value:"Other Movables"},
+        {code: "ZIN002" , value:"Inventory"},
+        {code: "ZOT001" , value:"Project Clearances"},
+        {code: "ZOT002" , value:"Project Documents"},
+        {code: "ZOT003" , value:"Bank Guarantees under project documents"},
+        {code: "ZOT004" , value:"Approvals"},
+        {code: "ZOT005" , value:"Claims from Insurances"},
+        {code: "ZOT006" , value:"Claims from letter of credit"},
+        {code: "ZPA001" , value:"Patents"},
+        {code: "ZRE001" , value:"Land"},
+        {code: "ZRE002" , value:"Building"}
     ];
 
     // collateralAgreementTypes
     static collateralAgreementTypes = [
-        {code: "0" , value:"Collateral Agreement Type 1"},
-        {code: "1" , value:"Collateral Agreement Type 2"}
+        {objectType: "ZRE001", code: "Z00001", value:"Mortgage"},
+        {objectType: "ZRE002", code: "Z00002", value:"Mortgage Registerd"},
+        {objectType: "Z30001", code: "Z00003", value:"Pledge Of Securities Account"},
+        {objectType: "ZPA001", code: "Z00005", value:"Pledge of Patents/Rights"},
+        {objectType: "ZCL001", code: "Z00004", value:"Pledge of Accounts"},
+        {objectType: "ZCL002", code: "Z00004", value:"Pledge of Accounts"},
+        {objectType: "ZCL003", code: "Z00004", value:"Pledge of Accounts"},
+        {objectType: "Z00007", code: "Z00007", value:"Personal Guarantee"},
+        {objectType: "Z00008", code: "Z00008", value:"Corporate Guarantee"},
+        {objectType: "Z00009", code: "Z00009", value:"Bank Guarantee"},
+        {objectType: "ZIN002", code: "Z00011", value:"Transfer Of Rights on Inventory"},
+        {objectType: "ZOT001", code: "Z00006", value:"Pledge of Other Valuables"},
+        {objectType: "ZOT002", code: "Z00006", value:"Pledge of Other Valuables"},
+        {objectType: "ZOT003", code: "Z00006", value:"Pledge of Other Valuables"},
+        {objectType: "ZOT004", code: "Z00006", value:"Pledge of Other Valuables"},
+        {objectType: "ZOT005", code: "Z00006", value:"Pledge of Other Valuables"},
+        {objectType: "ZOT006", code: "Z00011", value:"Transfer Of Rights on Inventory"},
+        {objectType: "ZDE001", code: "Z00010", value:"Hypothecation"},
+        {objectType: "ZDE002", code: "Z00010", value:"Hypothecation"},
+        {objectType: "ZDE003", code: "Z00010", value:"Hypothecation"},
+        {objectType: "ZDE004", code: "Z00010", value:"Hypothecation"}
     ];    
 
     // applicability
@@ -69,26 +107,46 @@ export class LoanMonitoringConstants {
 
     // actionPeriods
     static actionPeriods = [
-        {code: "0" , value:"Action Period 1"},
-        {code: "1" , value:"Action Period 2"}
+        {code: "1" , value:"Days"},
+        {code: "2" , value:"Weeks"},
+        {code: "3" , value:"Months"},
+        {code: "4" , value:"Years"}
     ];
 
     // actionPeriodSuffixes
     static actionPeriodSuffixes = [
-        {code: "0" , value:"Action Period Suffix 1"},
-        {code: "1" , value:"Action Period Suffix 2"}
+        {code: "1" , value:"Days"},
+        {code: "2" , value:"Weeks"},
+        {code: "3" , value:"Months"},
+        {code: "4" , value:"Years"}
     ];
 
     // eventTypes
     static eventTypes = [
-        {code: "0" , value:"Event Type 1"},
-        {code: "1" , value:"Event Type 2"}
+        {code: "1" , value:"Enquiry Completion Date"},
+        {code: "2" , value:"ICC Clearance Date"},
+        {code: "3" , value:"Appraisal Completion Date"},
+        {code: "4" , value:"Sanction Letter Issue Date"},
+        {code: "5" , value:"Contract Date"},
+        {code: "6" , value:"1st Disbursement Date"},
+        {code: "7" , value:"Scheduled COD"},
+        {code: "8" , value:"Actual COD"},
+        {code: "9" , value:"Date of Security"},
+        {code: "0" , value:"Others"}
     ];
 
     // unitOfMeasures
     static unitOfMeasures = [
-        {code: "0" , value:"Unit of Measure 1"},
-        {code: "1" , value:"Unit of Measure 2"}
+        {code: "ACR" , value:"Acre"},
+        {code: "CM2" , value:"Square centimeter"},
+        {code: "FT2" , value:"Square foot"},
+        {code: "HA" , value:"Hectare"},
+        {code: "KM2" , value:"Square kilometer"},
+        {code: "LHK" , value:"Liter per 100 km"},
+        {code: "M2" , value:"Square meter"},
+        {code: "MI2" , value:"Square mile"},
+        {code: "MM2" , value:"Square millimeter"},
+        {code: "YD2" , value:"Square Yard"}
     ];
 
     // particulars
