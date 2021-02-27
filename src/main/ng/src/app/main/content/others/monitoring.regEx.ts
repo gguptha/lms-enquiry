e3b01e1e06e30a0719116465206dd6f2649fa42d
Gopinath Guptha <gopinath.br@gmail.com>
2021-02-27 18:56:40 +0530

    LIE and LFA bug fixes ...

---
+++ src/main/ng/src/app/main/content/others/monitoring.regEx.ts
@@ -0,0 +1,5 @@
+export class MonitoringRegEx {
+
+    public static genericAmount = /^\d{1,4}(\.\d{1,2})?$/;
+    public static genericDate = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+} 
